1aa92ef8bcd221ee840b30d60bd65bc4ac621085
Ram Kolluri <kolluri.rk@gmail.com>
2018-09-19 13:25:15 -0400

    time netry controller with CRUD

---
+++ src/PalTracker/InMemoryTimeEntryRepository.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PalTracker
+{
+    public class InMemoryTimeEntryRepository : ITimeEntryRepository
+    {
+        private Dictionary<long, TimeEntry> _timeEntries = new Dictionary<long, TimeEntry>();
+
+        public bool Contains(long id) => _timeEntries.ContainsKey(id);
+
+        public TimeEntry Create(TimeEntry timeEntry)
+        {
+            var id = _timeEntries.Count + 1;
+            timeEntry.Id = id;
+            _timeEntries.Add(id, timeEntry);
+            return timeEntry;
+        }
+
+        public void Delete(long id)
+        {            
+            _timeEntries.Remove(id);
+        }
+
+        public TimeEntry Find(long id) => _timeEntries[id];
+
+        public IEnumerable<TimeEntry> List() => _timeEntries.Values.ToList();
+
+        public TimeEntry Update(long id, TimeEntry timeEntry)
+        {
+            timeEntry.Id = id;
+            _timeEntries[id] = timeEntry;
+            return timeEntry;
+        }
+    }
+}

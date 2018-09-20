07f2ea9db0b5fa44a83a9b24621680848345790d
Ram Kolluri <kolluri.rk@gmail.com>
2018-09-20 11:28:53 -0400

    ssh tunnel - added 2nd run to get through

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -17,9 +17,10 @@ echo "chek ssh enabled on app $1"
 cf ssh-enabled $1
 
 echo "sshing to app $1"
-cf ssh $1 
+
 
 echo "Opening ssh tunnel to $ip_address using app $1"
+cf ssh -N -L 63306:$ip_address:3306 $1 & || true
 cf ssh -N -L 63306:$ip_address:3306 $1 &
 cf_ssh_pid=$!
 
